28bca2f5e8528cd394f9070b5af2973dd0454815
aliass532 <56620619+aliass532@users.noreply.github.com>
2025-08-11 21:05:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i 's/192.168.1.1/192.168.21.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.21.1/g' package/base-files/files/bin/config_generate
 echo >> feeds.conf.default
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 # Add a feed source
